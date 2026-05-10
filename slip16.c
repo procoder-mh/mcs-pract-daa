c168b08fcd5ff8c0241504708fa2f460150b1020
procoder-mh <nikhildesai3112@gmail.com>
2026-05-10 23:08:04 +0530

    Update slip16.c

--- slip16.c
+++ slip16.c
@@ -1,125 +1,134 @@
-Slip 16
-Q1) 0/1 Knapsack using Dynamic Programming
-#include<stdio.h>
+SLIP 16
 
-int max(int a,int b)
-{
-    if(a>b)
-        return a;
 
-    return b;
+Q.1) Write a program to implement to find out solution for 0/1 knapsack problem using
+dynamic programming.
+
+
+#include <stdio.h>
+#define MAX 100
+ 
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
-int main()
-{
-    int i,w,n,m;
-    int p[10],wt[10],v[10][10];
+int main() {
+    int n, W;
 
-    printf("Enter number of items:");
-    scanf("%d",&n);
+    printf("Enter number of items: ");
+    scanf("%d", &n);
 
-    printf("Enter profits:\n");
-
-    for(i=1;i<=n;i++)
-        scanf("%d",&p[i]);
+    int wt[n], val[n];
 
     printf("Enter weights:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &wt[i]);
 
-    for(i=1;i<=n;i++)
-        scanf("%d",&wt[i]);
+    printf("Enter values:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &val[i]);
 
-    printf("Enter capacity:");
-    scanf("%d",&m);
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &W);
 
-    for(i=0;i<=n;i++)
-    {
-        for(w=0;w<=m;w++)
-        {
-            if(i==0 || w==0)
-                v[i][w]=0;
-
-            else if(wt[i]<=w)
-                v[i][w]=max(p[i]+v[i-1][w-wt[i]],v[i-1][w]);
-
+    int dp[n + 1][W + 1];
+ 
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                               dp[i - 1][w]);
             else
-                v[i][w]=v[i-1][w];
+                dp[i][w] = dp[i - 1][w];
         }
     }
 
-    printf("Maximum Profit=%d",v[n][m]);
+    printf("Maximum value = %d\n", dp[n][W]);
 
     return 0;
 }
-Q2) Hamiltonian Cycle
-#include<stdio.h>
 
-int x[10];
 
-int place(int k,int i,int graph[10][10])
-{
-    int j;
 
-    if(graph[x[k-1]][i]==0)
+Q.2) Write a program to determine if a given graph is a Hamiltonian cycle or not.
+
+
+#include <stdio.h>
+#define MAX 100
+
+int graph[MAX][MAX];
+int path[MAX];
+int V;
+ 
+int isSafe(int v, int pos) {
+    
+    if (graph[path[pos - 1]][v] == 0)
         return 0;
-
-    for(j=1;j<k;j++)
-    {
-        if(x[j]==i)
+ 
+    for (int i = 0; i < pos; i++) {
+        if (path[i] == v)
             return 0;
     }
 
     return 1;
 }
+ 
+int hamCycleUtil(int pos) {
+   
+    if (pos == V) {
+   
+        return graph[path[pos - 1]][path[0]] == 1;
+    }
+ 
+    for (int v = 1; v < V; v++) {
+        if (isSafe(v, pos)) {
+            path[pos] = v;
 
-void hamiltonian(int k,int n,int graph[10][10])
-{
-    int i,j;
+            if (hamCycleUtil(pos + 1))
+                return 1;
 
-    while(1)
-    {
-        x[k]=(x[k]+1)%(n+1);
-
-        if(x[k]==0)
-            return;
-
-        if(place(k,x[k],graph))
-        {
-            if(k==n)
-            {
-                if(graph[x[n]][x[1]]!=0)
-                {
-                    for(j=1;j<=n;j++)
-                        printf("%d ",x[j]);
-
-                    printf("%d",x[1]);
-                    printf("\n");
-                }
-            }
-            else
-                hamiltonian(k+1,n,graph);
+            path[pos] = -1; 
         }
     }
-}
-
-int main()
-{
-    int graph[10][10],i,j,n;
-
-    printf("Enter number of vertices:");
-    scanf("%d",&n);
-
-    printf("Enter adjacency matrix:\n");
-
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-            scanf("%d",&graph[i][j]);
-    }
-
-    x[1]=1;
-
-    hamiltonian(2,n,graph);
 
     return 0;
 }
 
+void hamCycle() {
+    for (int i = 0; i < V; i++)
+        path[i] = -1;
+
+    path[0] = 0;  
+
+    if (!hamCycleUtil(1)) {
+        printf("No Hamiltonian Cycle exists\n");
+        return;
+    }
+
+    printf("Hamiltonian Cycle:\n");
+    for (int i = 0; i < V; i++)
+        printf("%d ", path[i]);
+
+    printf("%d\n", path[0]); 
+}
+
+int main() {
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    hamCycle();
+
+    return 0;
+}
+
+
+____________________________________________________________________________
