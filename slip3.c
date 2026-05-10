a345a9cccca33811caf24ca55262807ceb98b15a
procoder-mh <nikhildesai3112@gmail.com>
2026-05-10 22:48:50 +0530

    Update slip3.c

--- slip3.c
+++ slip3.c
@@ -1,119 +1,136 @@
-Slip 3
-Q1) Quick Sort
+SLIP 3
+
+
+Q.1) Write a program to sort a given set of elements using the Quick sort method and determine
+the time required to sort the elements.
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = low - 1, j, temp;
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1, j, temp;
 
-    for(j = low; j < high; j++)
-    {
-        if(arr[j] < pivot)
-        {
-            i++;
-
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        for (j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
-    }
 
-    temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
 
-    return i + 1;
-}
-
-void quickSort(int arr[], int low, int high)
-{
-    if(low < high)
-    {
-        int pi = partition(arr, low, high);
+        int pi = i + 1;
 
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-int main()
-{
+int main() {
     int n, i;
-
-    scanf("%d",&n);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
     int arr[n];
 
-    for(i=0;i<n;i++)
-        scanf("%d",&arr[i]);
+    srand(time(0));
+    for (i = 0; i < n; i++) {
+        arr[i] = rand() % 1000;
+    }
 
-    quickSort(arr,0,n-1);
+    clock_t start = clock();
+    quickSort(arr, 0, n - 1);
+    clock_t end = clock();
 
-    for(i=0;i<n;i++)
-        printf("%d ",arr[i]);
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\nTime taken: %f seconds\n", time_taken);
 
     return 0;
 }
-Q2) Prim's Algorithm
-#include<stdio.h>
 
-int main()
-{
-    int cost[10][10], visited[10]={0};
-    int i,j,n,ne=1,min,a,b,u,v,mincost=0;
 
-    printf("Enter number of vertices:");
-    scanf("%d",&n);
 
-    printf("Enter cost matrix:\n");
+Q.2) Write a program to find Minimum Cost Spanning Tree of a given undirected graph using
+Prims algorithm
 
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            scanf("%d",&cost[i][j]);
 
-            if(cost[i][j]==0)
-                cost[i][j]=999;
-        }
+#include <stdio.h>
+#include <limits.h>
+
+#define V 10
+
+int minKey(int key[], int mstSet[], int n) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < n; v++)
+        if (mstSet[v] == 0 && key[v] < min)
+            min = key[v], min_index = v;
+
+    return min_index;
+}
+
+void primMST(int graph[V][V], int n) {
+    int parent[V];
+    int key[V];
+    int mstSet[V];
+
+    for (int i = 0; i < n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
     }
 
-    visited[1]=1;
+    key[0] = 0;
+    parent[0] = -1;
 
-    while(ne<n)
-    {
-        min=999;
+    for (int count = 0; count < n - 1; count++) {
+        int u = minKey(key, mstSet, n);
+        mstSet[u] = 1;
 
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=n;j++)
-            {
-                if(cost[i][j]<min)
-                {
-                    if(visited[i]!=0)
-                    {
-                        min=cost[i][j];
-                        a=u=i;
-                        b=v=j;
-                    }
-                }
+        for (int v = 0; v < n; v++)
+            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
             }
-        }
-
-        if(visited[u]==0 || visited[v]==0)
-        {
-            printf("\nEdge %d:(%d %d) cost:%d",ne++,a,b,min);
-
-            mincost+=min;
-            visited[b]=1;
-        }
-
-        cost[a][b]=cost[b][a]=999;
     }
 
-    printf("\nMinimum cost=%d",mincost);
+    int total = 0;
+    printf("Edge \tWeight\n");
+    for (int i = 1; i < n; i++) {
+        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
+        total += graph[i][parent[i]];
+    }
+    printf("Total Cost = %d\n", total);
+}
+
+int main() {
+    int n;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int graph[V][V];
+
+    printf("Enter adjacency matrix:\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d",&graph[i][j]);
+
+    primMST(graph, n);
 
     return 0;
 }
 
+
+______________________________________________________________________________________
