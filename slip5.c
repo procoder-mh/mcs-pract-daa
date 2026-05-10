b5a23709a59d7f72c78825fcc2626decc7977b9f
procoder-mh <nikhildesai3112@gmail.com>
2026-05-10 22:54:45 +0530

    Update slip5.c

--- slip5.c
+++ slip5.c
@@ -1,92 +1,232 @@
-Slip 5
-Q1) Kruskal Algorithm
-#include<stdio.h>
+SLIP 5
 
-int parent[10];
+Q.1) Write a program for the Implementation of Kruskal’s algorithm to find minimum
+cost spanning tree.
 
-int find(int i)
-{
-    while(parent[i])
-        i=parent[i];
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Edge {
+    int u, v, weight;
+};
+
+struct Edge edges[MAX];
+int parent[MAX];
+
+int find(int i) {
+    while (parent[i] != i)
+        i = parent[i];
     return i;
 }
 
-int uni(int i,int j)
-{
-    if(i!=j)
-    {
-        parent[j]=i;
-        return 1;
-    }
-
-    return 0;
+void union_set(int u, int v) {
+    int u_root = find(u);
+    int v_root = find(v);
+    parent[u_root] = v_root;
 }
 
-int main()
-{
-    int a,b,u,v,n,i,j,ne=1;
-    int min,mincost=0,cost[10][10];
-
-    printf("Enter vertices:");
-    scanf("%d",&n);
-
-    printf("Enter cost matrix:\n");
-
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            scanf("%d",&cost[i][j]);
-
-            if(cost[i][j]==0)
-                cost[i][j]=999;
-        }
-    }
-
-    while(ne<n)
-    {
-        min=999;
-
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=n;j++)
-            {
-                if(cost[i][j]<min)
-                {
-                    min=cost[i][j];
-                    a=u=i;
-                    b=v=j;
-                }
+void sortEdges(int e) {
+    for (int i = 0; i < e - 1; i++) {
+        for (int j = 0; j < e - i - 1; j++) {
+            if (edges[j].weight > edges[j + 1].weight) {
+                struct Edge temp = edges[j];
+                edges[j] = edges[j + 1];
+                edges[j + 1] = temp;
             }
         }
+    }
+}
 
-        u=find(u);
-        v=find(v);
+int main() {
+    int V, E;
 
-        if(uni(u,v))
-        {
-            printf("\n%d edge (%d,%d)=%d",ne++,a,b,min);
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
 
-            mincost+=min;
-        }
+    printf("Enter number of edges: ");
+    scanf("%d", &E);
 
-        cost[a][b]=cost[b][a]=999;
+    for (int i = 0; i < E; i++) {
+        printf("Enter edge %d (u v weight): ", i + 1);
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].weight);
     }
 
-    printf("\nMinimum cost=%d",mincost);
+    for (int i = 0; i < V; i++) {
+        parent[i] = i;
+    }
+
+    sortEdges(E);
+
+    int count = 0, i = 0;
+    int total_cost = 0;
+
+    printf("\nEdges in MST:\n");
+
+    while (count < V - 1 && i < E) {
+        int u = edges[i].u;
+        int v = edges[i].v;
+
+        int set_u = find(u);
+        int set_v = find(v);
+
+        if (set_u != set_v) {
+            printf("%d -- %d == %d\n", u, v, edges[i].weight);
+            total_cost += edges[i].weight;
+            union_set(set_u, set_v);
+            count++;
+        }
+        i++;
+    }
+
+    printf("Total cost of MST = %d\n", total_cost);
 
     return 0;
 }
-Q2) Huffman Coding
-#include<stdio.h>
 
-int main()
-{
-    printf("Huffman Coding using Greedy Method\n");
+.2) Write a program to implement Huffman Code using greedy methods and also calculate the
+best case and worst-case complexity.
 
-    printf("Best Case Complexity = O(n log n)\n");
-    printf("Worst Case Complexity = O(n log n)\n");
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    char data;
+    unsigned freq;
+    struct Node *left, *right;
+};
+
+struct MinHeap {
+    int size;
+    struct Node* array[MAX];
+};
+
+struct Node* newNode(char data, unsigned freq) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
+}
+
+void swap(struct Node** a, struct Node** b) {
+    struct Node* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void heapify(struct MinHeap* heap, int i) {
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < heap->size && heap->array[left]->freq < heap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < heap->size && heap->array[right]->freq < heap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != i) {
+        swap(&heap->array[i], &heap->array[smallest]);
+        heapify(heap, smallest);
+    }
+}
+
+struct Node* extractMin(struct MinHeap* heap) {
+    struct Node* temp = heap->array[0];
+    heap->array[0] = heap->array[heap->size - 1];
+    heap->size--;
+    heapify(heap, 0);
+    return temp;
+}
+
+void insertHeap(struct MinHeap* heap, struct Node* node) {
+    int i = heap->size;
+    heap->size++;
+
+    while (i && node->freq < heap->array[(i - 1)/2]->freq) {
+        heap->array[i] = heap->array[(i - 1)/2];
+        i = (i - 1)/2;
+    }
+    heap->array[i] = node;
+}
+
+struct Node* buildHuffman(char data[], int freq[], int n) {
+    struct MinHeap heap;
+    heap.size = n;
+
+    for (int i = 0; i < n; i++) {
+        heap.array[i] = newNode(data[i], freq[i]);
+    }
+
+    for (int i = (n-2)/2; i >= 0; i--) {
+        heapify(&heap, i);
+    }
+
+    while (heap.size > 1) {
+        struct Node* left = extractMin(&heap);
+        struct Node* right = extractMin(&heap);
+
+        struct Node* top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+
+        insertHeap(&heap, top);
+    }
+
+    return extractMin(&heap);
+}
+
+void printCodes(struct Node* root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+
+    if (!root->left && !root->right) {
+        printf("%c: ", root->data);
+        for (int i = 0; i < top; i++)
+            printf("%d", arr[i]);
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of characters: ");
+    scanf("%d", &n);
+
+    char data[n];
+    int freq[n];
+
+    printf("Enter characters:\n");
+    for (int i = 0; i < n; i++)
+        scanf(" %c", &data[i]);
+
+    printf("Enter frequencies:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &freq[i]);
+
+    struct Node* root = buildHuffman(data, freq, n);
+
+    int arr[MAX], top = 0;
+
+    printf("\nHuffman Codes:\n");
+    printCodes(root, arr, top);
 
     return 0;
 }
+
+
+___________________________________________________________________________________
